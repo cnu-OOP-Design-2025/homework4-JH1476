473d6ed906f6c5e1f2f036d4fca8e010a1a4b550
JH1476 <junghun14760@naver.com>
2025-10-08 03:04:22 +0900

    과제 제출

--- student_manager.cpp
+++ student_manager.cpp
@@ -6,50 +6,109 @@ int StudentManager::last_student_id = 1000;
 
 void StudentManager::addStudent(char const* name, float midterm, float final) {
     int id = ++last_student_id;
-    /* TODO */
-
+    students[num_of_students] = Student(name, id, midterm, final);
+    num_of_students++;
 }
 
 void StudentManager::deleteStudent(int id) {
-    /* TODO */
+    int idx = findStudentByID(id);
+
+    for(int i = idx; i < num_of_students-1; i++) {
+        students[i] = students[i+1];
+    }
+    num_of_students--;
 }
 
 void StudentManager::modifyStudent(const Student& student) {
-    /* TODO */
+    int id = student.getID();
+    int idx = findStudentByID(id);
+    students[idx] = student;
 }
 
 int StudentManager::findStudentByID(int id) const {
-    /* TODO */
+    for (int i = 0; i < num_of_students; i++) {
+        if(students[i].getID() == id) {
+            return i;
+        }
+    }
     return -1;
 }
 
 int StudentManager::findBestStudentInMidterm() const {
-    /* TODO */
+    if (num_of_students > 0) {
+    float max = 0.0f;
+    int best_idx = 0;
+    for (int i = 0; i < num_of_students; i++) {
+        if(students[i].getRecord().getMidterm() > max) {
+            max = students[i].getRecord().getMidterm();
+            best_idx = i;
+            }
+        }
+        return best_idx;
+    }
     return -1;
 }
 
 int StudentManager::findBestStudentInFinal() const {
-    /* TODO */
+    if (num_of_students > 0) {
+    float max = 0.0f;
+    int best_idx = 0;
+    for (int i = 0; i < num_of_students; i++) {
+        if(students[i].getRecord().getFinal() > max) {
+            max = students[i].getRecord().getFinal();
+            best_idx = i;
+            }
+        }
+        return best_idx;
+    }
     return -1;
 }
 
 int StudentManager::findBestStudent() const {
-    /* TODO */
+    if (num_of_students > 0) {
+    float max = 0.0f;
+    int best_id = 0;
+    for (int i = 0; i < num_of_students; i++) {
+        if(students[i].getRecord().getMidterm() + students[i].getRecord().getFinal() > max) {
+            max = students[i].getRecord().getMidterm() + students[i].getRecord().getFinal();
+            best_id = students[i].getID();
+            }
+        }
+        return best_id;
+    }
     return -1;
 }
 
 float StudentManager::getMidtermAverage() const {
-    /* TODO */
+    if(num_of_students > 0) {
+        float sum = 0.0f;
+        for(int i = 0; i < num_of_students; i++) {
+            sum += students[i].getRecord().getMidterm();
+        }
+        return sum/(float)num_of_students;
+    }
     return 0.0f;
 }
 
 float StudentManager::getFinalAverage() const {
-    /* TODO */
+    if(num_of_students > 0) {
+        float sum = 0.0f;
+        for(int i = 0; i < num_of_students; i++) {
+            sum += students[i].getRecord().getFinal();
+        }
+        return sum/(float)num_of_students;
+    }
     return 0.0f;
 }
 
 float StudentManager::getTotalAverage() const {
-    /* TODO */
+    if(num_of_students > 0) {
+        float sum = 0.0f;
+        for(int i = 0; i < num_of_students; i++) {
+            sum += (students[i].getRecord().getMidterm() + students[i].getRecord().getFinal());
+        }
+        return sum/(float)num_of_students;
+    }
     return 0.0f;
 }
 
